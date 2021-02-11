2f5f8a2d28ffe2be0d056465242618629362b865
Joao Gouveia <joao.gouveia@criticaltechworks.com>
2021-02-11 19:20:27 +0000

    ruby and java ok

--- solutions/ruby/counting_valleys.rb
+++ solutions/ruby/counting_valleys.rb
@@ -1,3 +1,16 @@
 def counting_valleys steps, path
-	return 1
+	current_level = 0 #starts at sea level
+	valleys = 0
+
+	path.split("").each do |step|
+		if step == 'U' && current_level == -1
+			valleys = valleys + 1
+		end
+		if step == 'D'
+			current_level = current_level - 1
+		else
+			current_level = current_level + 1
+		end		
+	end
+	valleys
 end
